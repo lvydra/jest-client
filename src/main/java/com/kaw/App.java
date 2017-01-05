ec50270e025a1b754fdd314d1dd54fbb863accd4
Lukas Vydra <lvydra@redhat.com>
2017-01-05 15:33:48 +0100

    Indexing record from csv added

--- src/main/java/com/kaw/App.java
+++ src/main/java/com/kaw/App.java
@@ -1,5 +1,6 @@
 package com.kaw;
 
+import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -7,14 +8,16 @@ import java.util.List;
 import org.elasticsearch.index.query.QueryBuilder;
 import org.elasticsearch.index.query.QueryBuilders;
 
+import com.opencsv.CSVReader;
+
 import io.searchbox.client.JestClient;
 import io.searchbox.client.JestClientFactory;
 import io.searchbox.client.JestResult;
 import io.searchbox.client.config.HttpClientConfig;
 
 public class App {
-    private static final String TYPE_NAME = "records";
-    private static final String INDEX_NAME = "record";
+    private static final String TYPE_NAME = "error";
+    private static final String INDEX_NAME = "error_db";
 	
     public static void main(String args[]) throws IOException{
         try {
@@ -28,12 +31,8 @@ public class App {
             
             final RecordDTO record1 = new RecordDTO("Record 1 - test 1");
             final RecordDTO record2 = new RecordDTO("Record 2 - test 2");
-            final RecordDTO record3 = new RecordDTO("Record 3 - test 3");
-            final RecordDTO record4 = new RecordDTO("Record 4 - test 4");
             
-            List<Object> sources = new ArrayList<Object>();
-            sources.add(record3);
-            sources.add(record4);
+            List<Object> sources = readRecordsFromCsv("error_stack_tace.csv");
             
             try {
             	bscOps.deleteIndex(INDEX_NAME);
@@ -48,7 +47,11 @@ public class App {
             	
             	Thread.sleep(2000);
             	
-            	QueryBuilder query = QueryBuilders.termQuery("record", "test");
+            	RecordDTO randomRec = (RecordDTO) sources.get(120);
+            	
+            	QueryBuilder query = QueryBuilders.matchPhraseQuery("record", randomRec.getRecord());
+            	
+            	
             	JestResult result = bscOps.queryData(INDEX_NAME, TYPE_NAME, query);
             	
             	List<RecordDTO> records = result.getSourceAsObjectList(RecordDTO.class);
@@ -62,5 +65,26 @@ public class App {
         } catch (Exception e) {
         	e.printStackTrace();
         }
+    }
+    
+    public static List<Object> readRecordsFromCsv(String path) {
+    	List<Object> sources = new ArrayList<Object>();
+    	
+    	String csvFile = path;
+
+        CSVReader reader = null;
+        try {
+            reader = new CSVReader(new FileReader(csvFile));
+            String[] line;
+            line = reader.readNext();
+            while ((line = reader.readNext()) != null) {
+            	RecordDTO record = new RecordDTO(line[1]);
+            	sources.add(record);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+         
+        return sources;
     }
 }
